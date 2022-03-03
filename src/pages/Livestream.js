0f4fc7916a9059d64af7bdf3bd5474acd139e29c
Disha Agarwalla <dishaagarwalla2001@gmail.com>
2022-03-04 03:00:35 +0530

    livestream chat with streamChat

--- src/pages/Livestream.js
+++ src/pages/Livestream.js
@@ -1,31 +1,26 @@
 import React, { useEffect, useState } from "react";
 
-// import "../App.scss";
+import "../App.scss";
 
-// import { ChatUpgrades } from "../components/ChatUpgrades/ChatUpgrades";
-// import { GamingChat } from "../components/GamingChat/GamingChat";
-// import { GamingChatPopUp } from "../components/GamingChat/GamingChatPopUp";
-// import { GamingFooter } from "../components/GamingFooter/GamingFooter";
-// import { GamingHeader } from "../components/GamingHeader/GamingHeader";
-// import { GamingVideo } from "../components/GamingVideo/GamingVideo";
+import { GamingChat } from "../components/Livechat/GamingChat/GamingChat";
 import Livepeer from "../components/Livepeer";
 
 const Livestream = ({ stream }) => {
-  //   const [isFullScreen, setIsFullScreen] = useState(false);
-  //   const [popUpText, setPopUpText] = useState("");
-  //   const [showMembers, setShowMembers] = useState(false);
-  //   const [showUpgrade, setShowUpgrade] = useState(false);
-  //   const [showPopUp, setShowPopUp] = useState(false);
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  const [popUpText, setPopUpText] = useState("");
+  const [showMembers, setShowMembers] = useState(false);
+  const [showUpgrade, setShowUpgrade] = useState(false);
+  const [showPopUp, setShowPopUp] = useState(false);
 
-  //   useEffect(() => {
-  //     const popUpTimer = () => {
-  //       setTimeout(() => {
-  //         setShowPopUp(false);
-  //         setPopUpText("");
-  //       }, 3000);
-  //     };
-  //     if (showPopUp) popUpTimer();
-  //   }, [showPopUp]);
+  useEffect(() => {
+    const popUpTimer = () => {
+      setTimeout(() => {
+        setShowPopUp(false);
+        setPopUpText("");
+      }, 3000);
+    };
+    if (showPopUp) popUpTimer();
+  }, [showPopUp]);
 
   return (
     <main>
@@ -33,7 +28,7 @@ const Livestream = ({ stream }) => {
         <div style={{ width: "75%" }}>
           <Livepeer stream={stream} />
         </div>
-        {/* <GamingChat
+        <GamingChat
           style={{ width: "30%" }}
           isFullScreen={isFullScreen}
           setIsFullScreen={setIsFullScreen}
@@ -43,7 +38,7 @@ const Livestream = ({ stream }) => {
           setShowUpgrade={setShowUpgrade}
           showMembers={showMembers}
           showUpgrade={showUpgrade}
-        /> */}
+        />
       </div>
     </main>
   );
