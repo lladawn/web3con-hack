eab71969203f6a4da197cb03bd2647c771d61307
Disha Agarwalla <dishaagarwalla2001@gmail.com>
2022-03-06 23:06:30 +0530

    poap image added and redeployed

--- src/ethereum/PoapNFT.js
+++ src/ethereum/PoapNFT.js
@@ -3,7 +3,8 @@ import abi from "./build/PoapNFT.json";
 
 const PoapNFT = new web3.eth.Contract(
   abi,
-  "0x21E8c23C3EE37D054d36f80F3459F283F3EfE97D" // rinkeby
+  // "0x21E8c23C3EE37D054d36f80F3459F283F3EfE97D" // rinkeby
+  "0x20885B709b78D1a91E19518CEb74711C14c3A6e0" // with image on rinkeby
 );
 
 export default PoapNFT;
